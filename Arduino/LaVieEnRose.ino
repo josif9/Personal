4b1f2982c078a829f2e7247ca1d4d9eba7af6ecc
josif9 <160293929+josif9@users.noreply.github.com>
2025-03-09 11:04:50 -0700

    Update LaVieEnRose.ino

--- Arduino/LaVieEnRose.ino
+++ Arduino/LaVieEnRose.ino
@@ -68,17 +68,13 @@ void loop()
   int size = sizeof(durations) / sizeof(int);
 
   for (int note = 0; note < size; note++) {
-    //to calculate the note duration, take one second divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+
     int duration = 2000 / durations[note];
     tone(BUZZER_PIN, melody[note], duration);
 
-    //to distinguish the notes, set a minimum time between them.
-    //the note's duration + 30% seems to work well:
     int pauseBetweenNotes = duration * 1.50;
     delay(pauseBetweenNotes);
 
-    //stop the tone playing:
     noTone(BUZZER_PIN);
   }
 }
